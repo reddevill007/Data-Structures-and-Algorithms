564c36b7ed04ba674fa6e0b40134740705f27184
reddevill007 <shivampandey2900@gmail.com>
2022-07-04 06:21:31 +0530

    formatted

--- Queue/02_queue_stl.cpp
+++ Queue/02_queue_stl.cpp
@@ -6,28 +6,28 @@ using namespace std;
 int main()
 {
 
-    // create a queue of string
-    queue<string> animals;
+  // create a queue of string
+  queue<string> animals;
 
-    // push elements into the queue
-    animals.push("Cat");
-    animals.push("Dog");
+  // push elements into the queue
+  animals.push("Cat");
+  animals.push("Dog");
 
-    cout << "Queue: ";
+  cout << "Queue: ";
 
-    // print elements of queue
-    // loop until queue is empty
-    while (!animals.empty())
-    {
+  // print elements of queue
+  // loop until queue is empty
+  while (!animals.empty())
+  {
 
-        // print the element
-        cout << animals.front() << ", ";
+    // print the element
+    cout << animals.front() << ", ";
 
-        // pop element from the queue
-        animals.pop();
-    }
+    // pop element from the queue
+    animals.pop();
+  }
 
-    cout << endl;
+  cout << endl;
 }
 
 /*
