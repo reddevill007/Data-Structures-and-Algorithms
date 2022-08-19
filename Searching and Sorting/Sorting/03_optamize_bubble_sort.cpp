fbdd3a16b6b4f6af5ef855c381d7b01028c68834
reddevill007 <shivampandey2900@gmail.com>
2022-08-19 10:02:06 +0530

    more optamized bubble sort

--- Searching and Sorting/Sorting/03_optamize_bubble_sort.cpp
+++ Searching and Sorting/Sorting/03_optamize_bubble_sort.cpp
@@ -2,27 +2,24 @@
 
 using namespace std;
 
-void optamize_bubble_sort(int arr[], int size)
+void optamize_bubble_sort(int arr[], int n)
 {
-    int flag = 0;
-    for (int i = 0; i < size - 1; i++) // outer loop for passes
+    bool swapped = false;
+    for (int i = 0; i < n - 1; i++)
     {
-        for (int j = 0; j < size - i - 1; j++) // inner loop for comparisons
+        for (int j = 0; j < n - 1 - i; j++)
         {
             if (arr[j] > arr[j + 1])
             {
-                int temp = arr[j];   // assigning  value of first element into temp
-                arr[j] = arr[j + 1]; // assigning value of second element into first one
-                arr[j + 1] = temp;   // assigning value of temp into second element
-                flag = 1;            // make the value of flag 1 indicating that interchanging took place in the current pass
+                swap(arr[j], arr[j + 1]);
+                swapped = true;
             }
         }
         /* no interchanging took place in the pass just concluded
            thus indicating that array has turned sorted, no need to go for more passes */
-        if (flag == 0)
-            break; // terminates the outer loop
-        else
-            flag = 0; // reinitialising value of flag with 0
+
+        if (swapped == false)
+            break;
     }
 }
 
