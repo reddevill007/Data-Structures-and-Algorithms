c6e17c9ddef7f91e9bed9c8e0aa921b1b827e540
reddevill007 <shivampandey2900@gmail.com>
2022-09-09 17:39:11 +0530

    power using recursion

---
+++ Recursion/11_power.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int power(int a, int b)
+{
+    if (b == 0)
+    {
+        return 1;
+    }
+    if (b == 1)
+    {
+        return a;
+    }
+
+    int ans = power(a, b / 2);
+
+    if (b % 2 == 0)
+    {
+        return ans * ans;
+    }
+    else
+    {
+        return a * (ans * ans);
+    }
+}
+
+int main()
+{
+    int a, b;
+    cin >> a >> b;
+
+    int ans = power(a, b);
+
+    cout << "Answer is: " << ans << endl;
+}
