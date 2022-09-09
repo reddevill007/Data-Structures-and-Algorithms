cb85e5523de8634107d57194a752ea4c3ba4057a
reddevill007 <shivampandey2900@gmail.com>
2022-09-09 09:47:57 +0530

    binary search updated

--- Recursion/08_binary_search.cpp
+++ Recursion/08_binary_search.cpp
@@ -4,11 +4,23 @@
 
 using namespace std;
 
+void printArr(int arr[], int s, int e)
+{
+    for (int i = s; i <= e; i++)
+    {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+}
+
 bool binarySearch(int arr[], int key, int s, int e)
 {
+    printArr(arr, s, e);
     if (s <= e)
     {
         int mid = s + (e - s) / 2;
+        cout << "Value of mid is: " << arr[mid] << endl
+             << endl;
         if (key == arr[mid])
         {
             return true;
@@ -24,10 +36,10 @@ bool binarySearch(int arr[], int key, int s, int e)
 
 int main()
 {
-    int arr[5] = {2, 4, 5, 8, 88};
-    int size = 5;
+    int arr[14] = {2, 4, 5, 8, 9, 12, 23, 34, 45, 55, 66, 77, 88, 99};
+    int size = 14;
 
-    bool ans = binarySearch(arr, 88, 0, size - 1);
+    bool ans = binarySearch(arr, 99, 0, size - 1);
 
     cout << ans << endl;
 }
