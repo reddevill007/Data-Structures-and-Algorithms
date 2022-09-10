79f26f1adcd6876699a9816146826b82c9088542
reddevill007 <shivampandey2900@gmail.com>
2022-09-10 21:34:54 +0530

    error fix quick sort

--- Recursion/14_quick_sort.cpp
+++ Recursion/14_quick_sort.cpp
@@ -8,7 +8,7 @@ int partition(int arr[], int s, int e)
 {
     int pivot = arr[s];
     int ct = 0;
-    for (int i = s + 1; i < e; i++)
+    for (int i = s + 1; i <= e; i++)
     {
         if (arr[i] <= pivot)
         {
@@ -25,7 +25,7 @@ int partition(int arr[], int s, int e)
     int i = s, j = e;
     while (i < pivotIndex && j > pivotIndex)
     {
-        while (arr[i] < pivot)
+        while (arr[i] <= pivot)
         {
             i++;
         }
