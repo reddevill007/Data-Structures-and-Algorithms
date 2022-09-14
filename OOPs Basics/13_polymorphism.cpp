260663de4adaa28480400ebe0a394cb8bd576f4b
reddevill007 <shivampandey2900@gmail.com>
2022-09-15 00:59:59 +0530

    ..

--- OOPs Basics/13_polymorphism.cpp
+++ OOPs Basics/13_polymorphism.cpp
@@ -40,6 +40,31 @@ public:
         int val2 = obj.a;
         cout << "Output: " << val1 - val2 << endl;
     }
+
+    void operator()()
+    {
+        cout << "I am bracket: " << this->a << endl;
+    }
+};
+
+// Run time polymorphism
+class Animal
+{
+public:
+    void speak()
+    {
+        cout << "Speaking..." << endl;
+    }
+};
+
+class Dog : public Animal
+{
+public:
+    // Method overriding
+    void speak()
+    {
+        cout << "Barking..." << endl;
+    }
 };
 
 int main()
@@ -53,4 +78,8 @@ int main()
     obj1.a = 4;
     obj2.a = 7;
     obj1 + obj2;
+    obj1();
+
+    Dog d;
+    d.speak();
 }
