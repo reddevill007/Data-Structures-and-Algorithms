87d01cb63836702acfb0e7cf261afc59eb6c039b
reddevill007 <shivampandey2900@gmail.com>
2022-09-14 06:38:11 +0530

    deep shallow copy

--- OOPs Basics/02_copy_constructor.cpp
+++ OOPs Basics/02_copy_constructor.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
+#include <string.h>
 
 using namespace std;
 
@@ -8,12 +9,14 @@ class Hero
 {
 public:
     // Property
+    char *name;
     int health;
     int rank;
 
     Hero()
     {
         cout << "Default constructor called" << endl;
+        name = new char[100];
     }
 
     Hero(int health)
@@ -27,10 +30,24 @@ public:
         this->rank = rank;
     }
 
+    // copy constructure -> & is used as pass by reference
+    Hero(Hero &temp)
+    {
+        // Deep copy by creating new memory block to the name
+        char *ch = new char[strlen(temp.name) + 1];
+        strcpy(ch, temp.name);
+        this->name = ch;
+
+        cout << "Copy constructor called" << endl;
+        this->health = temp.health;
+        this->rank = temp.rank;
+    }
+
     void print()
     {
-        cout << "Heath is: " << health << endl;
-        cout << "Rank is: " << rank << endl;
+        cout << "Name is: " << this->name << endl;
+        cout << "Heath is: " << this->health << endl;
+        cout << "Rank is: " << this->rank << endl;
     }
 
     int getHealth()
@@ -52,14 +69,34 @@ public:
     {
         rank = r;
     }
+
+    void setName(char name[])
+    {
+        strcpy(this->name, name);
+    }
 };
 
 int main()
 {
     Hero suresh(70, 15);
-    suresh.print();
+    // suresh.print();
 
     // copy constructor
     Hero ram(suresh);
-    ram.print();
+    // ram.print();
+
+    Hero hero1;
+    hero1.setHealth(65);
+    hero1.setRank(12);
+    char name[8] = "Hanuman";
+    hero1.setName(name);
+
+    // hero1.print();
+
+    Hero hero2(hero1);
+
+    // Hero 2 will also change this is known as shallow copy but if we make our own copy constructor then it will do deep copy
+    hero1.name[0] = 'G';
+    hero1.print();
+    hero2.print();
 }
