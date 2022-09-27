435a845c3747f56ed8790f7b6ed66af69aff2d5f
reddevill007 <shivampandey2900@gmail.com>
2022-09-27 15:54:36 +0530

    add two linked list

---
+++ Linked List/17_add_two_no_list.cpp
@@ -0,0 +1,316 @@
+#include <iostream>
+#include <vector>
+#include <map>
+#include <algorithm>
+
+using namespace std;
+
+class Node
+{
+public:
+    int data;
+    Node *next;
+
+    // Constructor
+    Node(int data)
+    {
+        this->data = data;
+        this->next = NULL;
+    }
+
+    // Distructor
+    ~Node()
+    {
+        int val = this->data;
+
+        // memory free
+        if (this->next != NULL)
+        {
+            delete next;
+            this->next = NULL;
+        }
+
+        cout << "Memory is free for node with data " << val << endl;
+    }
+};
+
+void insertAtHead(Node *&head, int data)
+{
+    // new node creation
+    Node *temp = new Node(data);
+    temp->next = head;
+    head = temp;
+}
+
+void insertAtEnd(Node *&tail, int data)
+{
+    // new node creation
+    Node *temp = new Node(data);
+    tail->next = temp;
+    tail = temp;
+}
+
+void insertAtPosition(Node *&head, Node *&tail, int data, int pos)
+{
+    // Insert at start
+    if (pos == 1)
+    {
+        insertAtHead(head, data);
+        return;
+    }
+
+    Node *temp = head;
+
+    int i = 1;
+    while (i < pos - 1)
+    {
+        temp = temp->next;
+        i++;
+    }
+
+    // insertion at end
+    if (temp->next == NULL)
+    {
+        insertAtEnd(tail, data);
+        return;
+    }
+
+    // Inserting new node
+    Node *newNode = new Node(data);
+    newNode->next = temp->next;
+    temp->next = newNode;
+}
+
+void deleteAtPosition(int pos, Node *&head, Node *&tail)
+{
+    // Deleting first node
+    if (pos == 1)
+    {
+        Node *temp = head;
+        head = head->next;
+
+        // memory free
+        temp->next = NULL;
+        delete temp;
+    }
+    else
+    {
+        // Deleting any middle node or last node
+        Node *curr = head;
+        Node *prev = NULL;
+
+        int cnt = 1;
+        while (cnt < pos)
+        {
+            prev = curr;
+            curr = curr->next;
+            cnt++;
+        }
+
+        // updating tail node
+        if (curr->next == NULL)
+        {
+            tail = prev;
+        }
+
+        prev->next = curr->next;
+        curr->next = NULL;
+        delete curr;
+    }
+}
+
+void deleteByValue(int val, Node *&head, Node *&tail)
+{
+    // Deleting first node
+    if (head->data == val)
+    {
+        Node *temp = head;
+        head = head->next;
+
+        // memory free
+        temp->next = NULL;
+        delete temp;
+    }
+    else
+    {
+        // Deleting any middle node or last node
+        Node *curr = head;
+        Node *prev = NULL;
+
+        while (curr->data != val)
+        {
+            prev = curr;
+            curr = curr->next;
+        }
+
+        // updating tail node
+        if (curr->next == NULL)
+        {
+            tail = prev;
+        }
+
+        prev->next = curr->next;
+        curr->next = NULL;
+        delete curr;
+    }
+}
+
+void print(Node *&head)
+{
+    Node *temp = head;
+    while (temp != NULL)
+    {
+        cout << temp->data << " -> ";
+        temp = temp->next;
+    }
+    cout << " X " << endl;
+}
+
+Node *reverse(Node *head)
+{
+    Node *curr = head;
+    Node *prev = NULL;
+    Node *next = NULL;
+
+    while (curr != NULL)
+    {
+        next = curr->next;
+        curr->next = prev;
+        prev = curr;
+        curr = next;
+    }
+    return prev;
+}
+
+void insertAtTail(Node *&head, Node *&tail, int data)
+{
+    Node *temp = new Node(data);
+    if (head == NULL)
+    {
+        head = temp;
+        tail = temp;
+        return;
+    }
+    else
+    {
+        tail->next = temp;
+        tail = temp;
+    }
+}
+
+struct Node *add(struct Node *first, struct Node *second)
+{
+    int carry = 0;
+    Node *ansHead = NULL;
+    Node *ansTail = NULL;
+
+    while (first != NULL && second != NULL)
+    {
+        // sum of two list
+        int sum = carry + first->data + second->data;
+
+        // find digit to insert
+        int digit = sum % 10;
+
+        // create node and add in ans linked list
+        insertAtTail(ansHead, ansTail, digit);
+
+        // updating carry
+        carry = sum / 10;
+
+        // increment pointers
+        first = first->next;
+        second = second->next;
+    }
+
+    while (first != NULL)
+    {
+        // sum of two list
+        int sum = carry + first->data;
+
+        // find digit to insert
+        int digit = sum % 10;
+
+        // create node and add in ans linked list
+        insertAtTail(ansHead, ansTail, digit);
+
+        // updating carry
+        carry = sum / 10;
+
+        // update pointer
+        first = first->next;
+    }
+
+    while (second != NULL)
+    {
+        // sum of two list
+        int sum = carry + second->data;
+
+        // find digit to insert
+        int digit = sum % 10;
+
+        // create node and add in ans linked list
+        insertAtTail(ansHead, ansTail, digit);
+
+        // updating carry
+        carry = sum / 10;
+
+        // update pointer
+        second = second->next;
+    }
+
+    while (carry != 0)
+    {
+        int sum = carry;
+
+        // find digit to insert
+        int digit = sum % 10;
+
+        // create node and add in ans linked list
+        insertAtTail(ansHead, ansTail, digit);
+
+        // updating carry
+        carry = sum / 10;
+    }
+    return ansHead;
+}
+
+// Function to add two numbers represented by linked list.
+struct Node *addTwoLists(struct Node *first, struct Node *second)
+{
+    // step 1 -> reverse input LL
+    first = reverse(first);
+    second = reverse(second);
+
+    // step 2 -> add 2 LL
+    Node *ans = add(first, second);
+
+    // step 3 -> reverse ans
+    ans = reverse(ans);
+
+    return ans;
+}
+
+int main()
+{
+    Node *node1 = new Node(1);
+    Node *head1 = node1;
+    Node *tail1 = node1;
+    Node *node2 = new Node(2);
+    Node *head2 = node2;
+    Node *tail2 = node2;
+    insertAtEnd(tail1, 3);
+    insertAtEnd(tail1, 5);
+    insertAtEnd(tail1, 7);
+    insertAtEnd(tail2, 2);
+    insertAtEnd(tail2, 4);
+    insertAtEnd(tail2, 6);
+
+    cout << "Linked list before merging: " << endl;
+    print(head1);
+    print(head2);
+
+    Node *ans = addTwoLists(head1, head2);
+    cout << "\nLinked list after merging: " << endl;
+    print(ans);
+}
