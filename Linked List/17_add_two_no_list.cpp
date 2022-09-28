c6b43737daba00a78ce78d4f3ecb488f8cd92791
reddevill007 <shivampandey2900@gmail.com>
2022-09-28 07:34:20 +0530

    optimized add function

--- Linked List/17_add_two_no_list.cpp
+++ Linked List/17_add_two_no_list.cpp
@@ -204,10 +204,22 @@ struct Node *add(struct Node *first, struct Node *second)
     Node *ansHead = NULL;
     Node *ansTail = NULL;
 
-    while (first != NULL && second != NULL)
+    while (first != NULL || second != NULL || carry != 0)
     {
+        int val1 = 0;
+        if (first != NULL)
+        {
+            val1 = first->data;
+        }
+
+        int val2 = 0;
+        if (second != NULL)
+        {
+            val2 = second->data;
+        }
+
         // sum of two list
-        int sum = carry + first->data + second->data;
+        int sum = carry + val1 + val2;
 
         // find digit to insert
         int digit = sum % 10;
@@ -218,63 +230,20 @@ struct Node *add(struct Node *first, struct Node *second)
         // updating carry
         carry = sum / 10;
 
-        // increment pointers
-        first = first->next;
-        second = second->next;
+        // updating pointers
+        if (first != NULL)
+        {
+            first = first->next;
+        }
+
+        if (second != NULL)
+        {
+            second = second->next;
+        }
     }
 
-    while (first != NULL)
-    {
-        // sum of two list
-        int sum = carry + first->data;
-
-        // find digit to insert
-        int digit = sum % 10;
-
-        // create node and add in ans linked list
-        insertAtTail(ansHead, ansTail, digit);
-
-        // updating carry
-        carry = sum / 10;
-
-        // update pointer
-        first = first->next;
-    }
-
-    while (second != NULL)
-    {
-        // sum of two list
-        int sum = carry + second->data;
-
-        // find digit to insert
-        int digit = sum % 10;
-
-        // create node and add in ans linked list
-        insertAtTail(ansHead, ansTail, digit);
-
-        // updating carry
-        carry = sum / 10;
-
-        // update pointer
-        second = second->next;
-    }
-
-    while (carry != 0)
-    {
-        int sum = carry;
-
-        // find digit to insert
-        int digit = sum % 10;
-
-        // create node and add in ans linked list
-        insertAtTail(ansHead, ansTail, digit);
-
-        // updating carry
-        carry = sum / 10;
-    }
     return ansHead;
 }
-
 // Function to add two numbers represented by linked list.
 struct Node *addTwoLists(struct Node *first, struct Node *second)
 {
