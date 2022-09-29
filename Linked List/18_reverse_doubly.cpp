0f19bbef4d7cb5b9e7dfd3a939b4310a556e4543
reddevill007 <shivampandey2900@gmail.com>
2022-09-29 09:27:53 +0530

    clone list

--- Linked List/18_reverse_doubly.cpp
+++ Linked List/18_reverse_doubly.cpp
@@ -1,4 +1,3 @@
-/* C++ program to reverse a doubly linked list */
 #include <iostream>
 #include <vector>
 #include <map>
@@ -6,7 +5,6 @@
 
 using namespace std;
 
-/* Node of the doubly linked list */
 class Node
 {
 public:
@@ -15,14 +13,11 @@ public:
     Node *prev;
 };
 
-/* Function to reverse a Doubly Linked List */
 void reverse(Node **head_ref)
 {
     Node *temp = NULL;
     Node *current = *head_ref;
 
-    /* swap next and prev for all nodes of
-    doubly linked list */
     while (current != NULL)
     {
         temp = current->prev;
@@ -31,58 +26,43 @@ void reverse(Node **head_ref)
         current = current->prev;
     }
 
-    /* Before changing the head, check for the cases like
-    empty list and list with only one node */
     if (temp != NULL)
+    {
         *head_ref = temp->prev;
+    }
 }
 
-/* UTILITY FUNCTIONS */
-/* Function to insert a node at the
-beginning of the Doubly Linked List */
 void push(Node **head_ref, int new_data)
 {
-    /* allocate node */
     Node *new_node = new Node();
 
-    /* put in the data */
     new_node->data = new_data;
-
-    /* since we are adding at the beginning,
-    prev is always NULL */
     new_node->prev = NULL;
-
-    /* link the old list off the new node */
     new_node->next = (*head_ref);
 
-    /* change prev of head node to new node */
     if ((*head_ref) != NULL)
+    {
         (*head_ref)->prev = new_node;
+    }
 
-    /* move the head to point to the new node */
     (*head_ref) = new_node;
 }
 
-/* Function to print nodes in a given doubly linked list
-This function is same as printList() of singly linked list
-*/
 void printList(Node *node)
 {
     while (node != NULL)
     {
-        cout << node->data << " ";
+        cout << node->data << " -> ";
         node = node->next;
     }
+    cout << " -> X " << endl;
 }
 
-// Driver's code
 int main()
 {
-    /* Start with the empty list */
+
     Node *head = NULL;
 
-    /* Let us create a sorted linked list to test the
-    functions Created linked list will be 10->8->4->2 */
     push(&head, 2);
     push(&head, 4);
     push(&head, 8);
@@ -91,7 +71,6 @@ int main()
     cout << "Original Linked list" << endl;
     printList(head);
 
-    // Function call
     reverse(&head);
 
     cout << "\nReversed Linked list" << endl;
@@ -99,5 +78,3 @@ int main()
 
     return 0;
 }
-
-// This code is contributed by rathbhupendra
