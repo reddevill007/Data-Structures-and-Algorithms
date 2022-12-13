4dce7023ae8a404887e80e731b231a8fd522eee3
reddevill007 <shivampandey2900@gmail.com>
2022-12-13 15:44:36 +0530

    binary tree basic

--- Binary Tree/01_creation.cpp
+++ Binary Tree/01_creation.cpp
@@ -122,9 +122,47 @@ void preorder(node *root)
     preorder(root->right);
 }
 
+void buildFromLvelOrder(node *&root)
+{
+    queue<node *> q;
+
+    cout << "Enter data for root" << endl;
+    int data;
+    cin >> data;
+    root = new node(data);
+    q.push(root);
+
+    while (!q.empty())
+    {
+        node *temp = q.front();
+        q.pop();
+
+        cout << "Enter left node for " << temp->data << endl;
+        int leftData;
+        cin >> leftData;
+
+        if (leftData != -1)
+        {
+            temp->left = new node(leftData);
+            q.push(temp->left);
+        }
+
+        cout << "Enter right node for " << temp->data << endl;
+        int rightData;
+        cin >> rightData;
+
+        if (rightData != -1)
+        {
+            temp->right = new node(rightData);
+            q.push(temp->right);
+        }
+    }
+}
+
 int main()
 {
     node *root = NULL;
+    /*
 
     // creating a tree
     root = buildTree(root);
@@ -146,4 +184,9 @@ int main()
     cout << endl
          << "PostOrder Traversal" << endl;
     postorder(root);
+    */
+
+    buildFromLvelOrder(root);
+    levelOrderTraversal(root);
+    // 1 3 5 7 11 17 -1 -1 -1 -1 -1 -1 -1
 }
