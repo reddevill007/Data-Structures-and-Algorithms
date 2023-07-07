d38144f5ea8a22e46cbb1f0c22f7d250c11da6f4
reddevill007 <shivampandey2900@gmail.com>
2023-07-07 18:41:02 +0530

    ninjaTraining constant space

--- Dynamic Programming/05_ninja_training.cpp
+++ Dynamic Programming/05_ninja_training.cpp
@@ -50,3 +50,32 @@ int ninjaTraining(int n, vector<vector<int>> &points)
 
     return f(n - 1, 3, points, dp);
 }
+
+int ninjaTraining2(int n, vector<vector<int>> &points)
+{
+    vector<int> dp(4, 0);
+
+    dp[0] = max(points[0][1], points[0][2]);
+    dp[1] = max(points[0][0], points[0][2]);
+    dp[2] = max(points[0][1], points[0][0]);
+    dp[3] = max(points[0][1],max(points[0][0], points[0][2]);
+
+    for(int day = 1; day < n; day++) {
+        vector<int> temp(4, 0);
+
+        for (int last = 0; last < 4; last; last++)
+        {
+            temp[last] = 0;
+
+            for (int task = 0; task < 3; task++)
+            {
+                if (task != last)
+                {
+                    temp[last] = max(temp[last], points[day][task] + dp[task]);
+                }
+            }
+        }
+    }
+
+    return dp[3];
+}
